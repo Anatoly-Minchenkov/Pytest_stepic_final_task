cd2db5ad26392b372c438ddfdae2ae5f349f62c0
Anatoly-Minchenkov <Anatoly.Minchenkov@gmail.com>
2023-03-23 19:44:52 +0400

    finish project

--- Pages/base_page.py
+++ Pages/base_page.py
@@ -1,11 +1,19 @@
 from selenium.common.exceptions import NoSuchElementException
-
+from .locators import MainPageLocators, BasePageLocators
 class BasePage():
 
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url) #для первого подхода перехода страниц
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def open(self):
         self.browser.get(self.url)
--- test_main_page.py
+++ test_main_page.py
@@ -5,12 +5,20 @@ from selenium.webdriver.common.by import By
 from .Pages.main_page import MainPage
 from .Pages.locators import MainPageLocators
 from .Pages.login_page import LoginPage
+from .Pages.product_page import ProductPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()
+    page.should_be_login_link()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
