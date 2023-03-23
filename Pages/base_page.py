f4fffadf1937c5518b4f023c1f3925d452fe3826
Anatoly-Minchenkov <Anatoly.Minchenkov@gmail.com>
2023-03-23 18:19:02 +0400

    added tests for login page

--- Pages/base_page.py
+++ Pages/base_page.py
@@ -1,8 +1,25 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
 
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def bool_returner(self, element, error = NoSuchElementException):
+        try:
+            element
+        except error:
+            return False
+        return True
+
+    def is_element_present(self, how, what):
+        method = self.browser.find_element(how, what)
+        return self.bool_returner(method)
+    def is_word_in_url(self, word):
+        method = word in self.browser.current_url
+        return self.bool_returner(method)
--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,16 @@
+import time
+
 import pytest
 from selenium.webdriver.common.by import By
 from .Pages.main_page import MainPage
+from .Pages.locators import MainPageLocators
+from .Pages.login_page import LoginPage
 
-
-link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
+    page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page
